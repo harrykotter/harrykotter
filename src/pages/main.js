ba0455f8f8c1b39e110d5a6116eb7c9e6be11909
Harry K <155596644+harrykotter@users.noreply.github.com>
2024-11-05 18:59:34 +0900

    Feature: Add Social network contact link

--- src/pages/main.js
+++ src/pages/main.js
@@ -8,8 +8,27 @@ const Main = () => {
     <Feature>
       <header className="header">
         <img alt="profile" src="profile.png" className="profile_img" />
-        <div className="profile_text">
-          Hi, I'm Harry. I'm a junior publisher! Always seeking for new events!
+        <div className="profile_info">
+          <div className="profile_text">
+            Hi, I'm Harry. I'm a junior publisher! Always seeking for new
+            events!
+          </div>
+          <div className="sns">
+            <a
+              href="https://www.instagram.com/0hooni_98/"
+              target="_blank"
+              rel="noopnener noreferrer"
+            >
+              Instagram
+            </a>
+            <a
+              href="https://www.instagram.com/0hooni_98/"
+              target="_blank"
+              rel="noopnener noreferrer"
+            >
+              Blog
+            </a>
+          </div>
         </div>
       </header>
       <div className="project_wrapper">
